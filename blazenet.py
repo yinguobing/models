d2f51bbd46f2d1e20c363efdf0cf4f907839023e
yinguobing <yinguobing@gmail.com>
2020-10-29 11:34:25 +0800

    add double blaze block

--- blazenet.py
+++ blazenet.py
@@ -47,3 +47,44 @@ def blaze_block(filters, strides=(1, 1)):
         return layers.Activation("relu")(shortcut)
 
     return forward
+
+
+def double_blaze_block(filters_1, filters_2, strides=(1, 1)):
+    block_layers = [
+        layers.DepthwiseConv2D(kernel_size=(5, 5),
+                               strides=strides,
+                               padding='same'),
+        layers.Conv2D(filters=filters_1,
+                      kernel_size=(1, 1),
+                      padding='same'),
+        layers.BatchNormalization(),
+        layers.Activation("relu"),
+        layers.DepthwiseConv2D(kernel_size=(5, 5),
+                               padding='same'),
+        layers.Conv2D(filters=filters_2,
+                      kernel_size=(1, 1),
+                      padding='same'),
+        layers.BatchNormalization()]
+
+    def forward(inputs):
+        x = inputs
+        for layer in block_layers:
+            x = layer(x)
+
+        # Optional layers
+        inputs = layers.MaxPool2D(pool_size=(5, 5),
+                                  strides=strides,
+                                  padding='same')(inputs)
+        channels_to_pad = filters_2 - inputs.shape.as_list()[3]
+
+        if channels_to_pad > 0:
+            inputs = zero_channel_padding(channels_to_pad)(inputs)
+        elif channels_to_pad < 0:
+            inputs = layers.Conv2D(filters_2, (1, 1))(inputs)
+            inputs = layers.BatchNormalization()(inputs)
+
+        # Shortcut connection.
+        shortcut = layers.Add()([x, inputs])
+        return layers.Activation("relu")(shortcut)
+
+    return forward
--- blazenet_test.py
+++ blazenet_test.py
@@ -34,6 +34,12 @@ class TestTensorShapes(unittest.TestCase):
         self.assertListEqual(outputs.shape.as_list(),
                              [None, self.height, self.width, self.filters - 3])
 
+    def test_double_blaze_block_downsample(self):
+        outputs = double_blaze_block(
+            self.filters - 3, self.filters + 3, (2, 2))(self.inputs)
+        self.assertListEqual(outputs.shape.as_list(),
+                             [None, self.height/2, self.width/2, self.filters + 3])
+
 
 if __name__ == "__main__":
     unittest.main()
